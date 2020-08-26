f8f7b8a22ad02261d7197ff68982ced60bbcb1b5
Sam McCall <smccall@smccall.com>
2020-08-25 22:00:11 -0400

    Reorganize home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,5 @@
 <template>
   <div>
-    <div class="section">
-      <figure class="image is-16by9">
-        <img src="../assets/motw-cover.jpg">
-      </figure>
-    </div>
     <div class="content">
       <p>
         Hark, fellow keeper of stories! Are there too many Bystanders and NPCS crowding your notes that you can't keep track of them? Look no further!
@@ -13,8 +8,13 @@
         Creating a new Bystander can be done in seconds, and you can access your notes for each character any time you like.
       </p>
       <p>
-        Go ahead and get started! Why don't you go ahead and <router-link to="/create">create a Bystander</router-link> now?
+        Ready to create some civilians who will inevitably get in the way of our daring hunters? Let's get started! Go ahead and <router-link to="/create">create a Bystander</router-link> so you can get your mystery on track.
       </p>
+    </div>
+    <div class="">
+      <figure class="image is-16by9">
+        <img src="../assets/motw-cover.jpg">
+      </figure>
     </div>
   </div>
 </template>
