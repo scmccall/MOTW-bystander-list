42e535e5828c0fbed803d8de66dec8c41f401345
Sam McCall <smccall@smccall.com>
2020-08-25 21:59:52 -0400

    Typo fixes

--- src/components/WhatIsMOTW.vue
+++ src/components/WhatIsMOTW.vue
@@ -5,7 +5,7 @@
         <strong>Monster of the Week</strong> is a standalone action-horror tabletop RPG for 3-5 people. Hunt high school beasties a la Buffy the Vampire Slayer, travel the country to bring down unnatural creatures like the Winchester brothers of Supernatural, or head up the government investigation like Mulder and Scully.
       </p>
       <p>
-        During play, you'll embark on a mystery led by a <strong>Keeper</strong> who informs you abour the world your character is interacting with. Together with your fellow <strong>Hunter</strong> players, you'll search for clue, interview suspects, and kick some monster butt- all while making sure you don't die along the way!
+        During play, you'll embark on a mystery led by a <strong>Keeper</strong> who informs you about the world your character is interacting with. Together with your fellow <strong>Hunter</strong> players, you'll search for clue, interview suspects, and kick some monster butt- all while making sure you don't die along the way!
       </p>
       <p>
         All you need to play are two 6-sided dice, a pencil, and your all the tropes you can think of. If you're interested in learning more or getting started, you can check out the game's <a href="https://www.evilhat.com/home/monster-of-the-week/">website</a> where you can find all sorts of materials to help you plan your first mystery.
